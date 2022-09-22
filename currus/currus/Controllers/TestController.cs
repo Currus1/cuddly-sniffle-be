99dcb077e78185f68fb10796ba385fae7e4ef0bd
Tedas <tedodarbo@gmail.com>
2022-09-22 12:09:39 +0300

    base backend

---
+++ currus/currus/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace currus.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello, this is asp.net backend";
+        }
+    }
+}
