02b3308c82ed13cd965c4657c1d98a97a7a3dad9
Tedas <tedodarbo@gmail.com>
2022-09-30 18:50:53 +0300

    tarpai

--- currus/currus/Repository/TripRepository.cs
+++ currus/currus/Repository/TripRepository.cs
@@ -3,8 +3,6 @@ using System.Reflection.Metadata.Ecma335;
 
 namespace currus.Repository
 {
-
-
     public class TripRepository
     {
         static public List<OngoingTrip> ongoingTrips = new List<OngoingTrip>();
