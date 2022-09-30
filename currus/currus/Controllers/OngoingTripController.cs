18a0a5bc6f3676d81955c38a6ec159766c5074fb
Tedas <tedodarbo@gmail.com>
2022-09-30 18:46:31 +0300

    užduotis 600 (Struct)

---
+++ currus/currus/Controllers/OngoingTripController.cs
@@ -0,0 +1,26 @@
+﻿using currus.Models;
+using currus.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace currus.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OngoingTripController : Controller
+    {
+        [HttpGet]
+        [Route("trips")]
+        public List<OngoingTrip> Get()
+        {
+            return TripRepository.ongoingTrips;
+        }
+
+        [HttpPost]
+        [Route("adding")]
+        public void AddUser([FromBody] OngoingTrip ongoingTrip)
+        {
+            TripRepository.ongoingTrips.Add(ongoingTrip);
+        }
+    }
+}
---
+++ currus/currus/Repository/TripRepository.cs
@@ -0,0 +1,13 @@
+﻿using currus.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace currus.Repository
+{
+
+
+    public class TripRepository
+    {
+        static public List<OngoingTrip> ongoingTrips = new List<OngoingTrip>();
+
+    }
+}
