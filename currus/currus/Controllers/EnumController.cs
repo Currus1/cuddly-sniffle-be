1b9d44da04e63d3132435f7b76c8c501cea30303
Tedas <tedodarbo@gmail.com>
2022-10-02 14:45:51 +0300

    enum controller and trip status enum

---
+++ currus/currus/Controllers/EnumController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static currus.Enums.Enumerators;
+
+namespace currus.Controllers
+{
+    public class EnumController : Controller
+    {
+        public TripStatus tripStatus;
+
+        [HttpGet]
+        [Route("TripStatus")]
+        public TripStatus[] GetEnum()
+        {
+            return (TripStatus[])Enum.GetValues(typeof(TripStatus)).Cast<TripStatus>();
+        }
+    }
+}
