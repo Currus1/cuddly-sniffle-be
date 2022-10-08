3d1cff0528e3dc540bf1acc22454d979015b80e7
Tedas <tedodarbo@gmail.com>
2022-10-08 21:36:12 +0300

    optional and named arguments

--- currus/currus/Repository/DriverFileRepository.cs
+++ currus/currus/Repository/DriverFileRepository.cs
@@ -1,4 +1,5 @@
-﻿using currus.Models;
+﻿using currus.Enums;
+using currus.Models;
 using Newtonsoft.Json;
 
 namespace currus.Repository;
@@ -28,5 +29,18 @@ public class DriverFileRepository : FileRepository<Driver>, IDriverFileRepositor
                      orderby driver.Id ascending
                      select driver;
         return _sortQuery;
+    }
+
+    public Driver CheckVehicleType(Driver driver, string defaultVehicleType = "Sedan")
+    {
+        foreach (var type in VehicleTypes.GetValues<VehicleTypes>())
+        {
+            if (driver.VehicleType == type.ToString())
+            {
+                return driver;
+            }
+        }
+        driver.VehicleType = defaultVehicleType;
+        return driver;
     }
 }
