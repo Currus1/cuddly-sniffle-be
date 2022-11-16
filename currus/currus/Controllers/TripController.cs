f70a862f1bae09227453151e31bda9ced64a9b51
Pijus Zlatkus <zlatkus.pijus@gmail.com>
2022-11-16 13:24:32 +0200

    added delegate with generics

--- currus/currus/Controllers/TripController.cs
+++ currus/currus/Controllers/TripController.cs
@@ -12,10 +12,13 @@ namespace currus.Controllers;
 public class TripController : Controller
 {
     private readonly ITripDbRepository _tripDbRepository;
+    private readonly UserStatusChanger _userStatusChanger;
 
     public TripController(ITripDbRepository tripDbRepository)
     {
         _tripDbRepository = tripDbRepository;
+        _userStatusChanger = new UserStatusChanger();
+
     }
 
     [HttpPost]
@@ -25,16 +28,14 @@ public class TripController : Controller
         int basePrice = trip.CalculateBasePrice(trip); 
         trip.EstimatedTripPrice = trip.CalculateTripPrice(trip.Hours, trip.Minutes, trip.Distance, basePrice);
 
-        UserStatusEventHandler.ChangeEventHandler(trip, true);
+        _userStatusChanger.ChangeEventHandler(trip, true);
 
-        if (UserStatusEventHandler.UserStatusChanged != null)
-        {
-            if (trip.TripStatus == "Planned" || trip.TripStatus == "Ongoing")
-                UserStatusEventHandler.OnUserStatusChanged(this, new UserStatusEventArgs(true));
-            else
-                UserStatusEventHandler.OnUserStatusChanged(this, new UserStatusEventArgs(false));
-            UserStatusEventHandler.ChangeEventHandler(trip, false);
-        }
+        
+        if (trip.TripStatus == "Planned" || trip.TripStatus == "Ongoing")
+            _userStatusChanger.OnStatusChanged(this, new UserStatusEventArgs(true));
+        else
+            _userStatusChanger.OnStatusChanged(this, new UserStatusEventArgs(false));
+        _userStatusChanger.ChangeEventHandler(trip, false);
 
         await _tripDbRepository.Add(trip);
         await _tripDbRepository.SaveAsync();
@@ -45,13 +46,10 @@ public class TripController : Controller
     [Route("Deletion")]
     public async Task<IActionResult> DeleteTrip([FromBody] Trip trip)
     {
-        UserStatusEventHandler.ChangeEventHandler(trip, true);
+        _userStatusChanger.ChangeEventHandler(trip, true);
 
-        if (UserStatusEventHandler.UserStatusChanged != null)
-        {
-            UserStatusEventHandler.OnUserStatusChanged(this, new UserStatusEventArgs(false));
-            UserStatusEventHandler.ChangeEventHandler(trip, false);
-        }
+        _userStatusChanger.OnStatusChanged(this, new UserStatusEventArgs(false));
+        _userStatusChanger.ChangeEventHandler(trip, false);
 
         _tripDbRepository.Delete(trip);
         await _tripDbRepository.SaveAsync();
@@ -63,13 +61,10 @@ public class TripController : Controller
     public async Task<IActionResult> DeleteTripById(int id)
     {
         Trip trip = _tripDbRepository.Get(id);
-        UserStatusEventHandler.ChangeEventHandler(trip, true);
+        _userStatusChanger.ChangeEventHandler(trip, true);
 
-        if (UserStatusEventHandler.UserStatusChanged != null)
-        {
-            UserStatusEventHandler.OnUserStatusChanged(this, new UserStatusEventArgs(false));
-            UserStatusEventHandler.ChangeEventHandler(trip, false);
-        }
+        _userStatusChanger.OnStatusChanged(this, new UserStatusEventArgs(false));
+        _userStatusChanger.ChangeEventHandler(trip, false);
 
         _tripDbRepository.DeleteById(id);
         await _tripDbRepository.SaveAsync();
@@ -83,26 +78,20 @@ public class TripController : Controller
         var oldTrip = _tripDbRepository.GetTripAsNotTracked(trip.Id); 
         _tripDbRepository.Update(trip);
 
-        UserStatusEventHandler.ChangeEventHandler(oldTrip, true);
-        if (UserStatusEventHandler.UserStatusChanged != null)
-        {
-            UserStatusEventHandler.OnUserStatusChanged(this, new UserStatusEventArgs(false));
-            UserStatusEventHandler.ChangeEventHandler(oldTrip, false);
-        }
-        UserStatusEventHandler.ChangeEventHandler(trip, true);
+        _userStatusChanger.ChangeEventHandler(oldTrip, true);
+        _userStatusChanger.OnStatusChanged(this, new UserStatusEventArgs(false));
+        _userStatusChanger.ChangeEventHandler(oldTrip, false);
+        _userStatusChanger.ChangeEventHandler(trip, true);
 
-        if (UserStatusEventHandler.UserStatusChanged != null)
+        if (trip.TripStatus == "Planned" || trip.TripStatus == "Ongoing")
         {
-            if (trip.TripStatus == "Planned" || trip.TripStatus == "Ongoing")
-            {
-                UserStatusEventHandler.OnUserStatusChanged(this, new UserStatusEventArgs(true));
-            }
-            else if (trip.TripStatus == "Ended" || trip.TripStatus == "Cancelled")
-            {
-                UserStatusEventHandler.OnUserStatusChanged(this, new UserStatusEventArgs(false));
-            }
-            UserStatusEventHandler.ChangeEventHandler(trip, false);
+            _userStatusChanger.OnStatusChanged(this, new UserStatusEventArgs(true));
         }
+        else if (trip.TripStatus == "Ended" || trip.TripStatus == "Cancelled")
+        {
+            _userStatusChanger.OnStatusChanged(this, new UserStatusEventArgs(false));
+        }
+        _userStatusChanger.ChangeEventHandler(trip, false);
 
         await _tripDbRepository.SaveAsync();
         return Ok(trip);
@@ -130,16 +119,14 @@ public class TripController : Controller
         var trip = _tripDbRepository.SetRelation(id, userId);
         _tripDbRepository.Update(trip);
 
-        UserStatusEventHandler.ChangeEventHandler(trip, true);
+        _userStatusChanger.ChangeEventHandler(trip, true);
 
-        if (UserStatusEventHandler.UserStatusChanged != null)
+        if (trip.TripStatus == "Planned" || trip.TripStatus == "Ongoing")
         {
-            if (trip.TripStatus == "Planned" || trip.TripStatus == "Ongoing")
-            {
-                UserStatusEventHandler.OnUserStatusChanged(this, new UserStatusEventArgs(true));
-            }
-            UserStatusEventHandler.ChangeEventHandler(trip, false);
+            _userStatusChanger.OnStatusChanged(this, new UserStatusEventArgs(true));
         }
+
+        _userStatusChanger.ChangeEventHandler(trip, false);
 
         await _tripDbRepository.SaveAsync();
         return Ok();
