801d4ed53a0f18d01dc9631b0d2a2b081536e35c
Pijus Zlatkus <zlatkus.pijus@gmail.com>
2022-12-05 20:02:05 +0200

    interceptor fix

--- currus/currus/Logging/Logic/ExceptionLogger.cs
+++ currus/currus/Logging/Logic/ExceptionLogger.cs
@@ -3,9 +3,9 @@ using Newtonsoft.Json;
 
 namespace currus.Logging.Logic
 {
-    public class ExceptionLogger : IAsyncInterceptor
+    public class ExceptionLogger : IInterceptor
     {
-        public void InterceptSynchronous(IInvocation invocation)
+        /public void Intercept(IInvocation invocation)
         {
             try
             {
@@ -23,51 +23,6 @@ namespace currus.Logging.Logic
                     $"Trace: {ex.StackTrace}");
                 throw;
             }
-        }
-
-        public void InterceptAsynchronous(IInvocation invocation)
-        {
-            invocation.ReturnValue = InternalInterceptAsynchronous(invocation);
-        }
-
-        private async Task InternalInterceptAsynchronous(IInvocation invocation)
-        {
-            try
-            {
-                Logger.LogInfo($"Method {invocation.Method.Name} called:\n" +
-                    $"  Parameters: {JsonConvert.SerializeObject(invocation.Arguments)}");
-
-                invocation.Proceed();
-                var task = (Task)invocation.ReturnValue;
-                await task;
-
-                Logger.LogInfo($"Method {invocation.Method.Name} response: {JsonConvert.SerializeObject(invocation.ReturnValue)}");
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError($"Catched in {invocation.Method} method:\n" +
-                    $"Error: {ex.Message}\n" +
-                    $"Trace: {ex.StackTrace}");
-                throw;
-            }
-        }
-
-        public void InterceptAsynchronous<TResult>(IInvocation invocation)
-        {
-            invocation.ReturnValue = InternalInterceptAsynchronous<TResult>(invocation);
-        }
-
-        private async Task<TResult> InternalInterceptAsynchronous<TResult>(IInvocation invocation)
-        {
-            // Step 1. Do something prior to invocation.
-
-            invocation.Proceed();
-            var task = (Task<TResult>)invocation.ReturnValue;
-            TResult result = await task;
-
-            // Step 2. Do something after invocation.
-
-            return result;
         }
     }
 }
