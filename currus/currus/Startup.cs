9edfb49d3005ecc8643af7b381ca3cf2f5ea5ea3
Pijus Zlatkus <zlatkus.pijus@gmail.com>
2022-12-05 20:12:59 +0200

    deleted comments

--- currus/currus/Startup.cs
+++ currus/currus/Startup.cs
@@ -48,8 +48,6 @@ namespace currus
                 options.UseSqlServer(connectionString)
                 );
 
-            //services.AddScoped<ITripDbRepository, TripDbRepository>();
-
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -124,92 +122,3 @@ namespace currus
         }
     }
 }
-
-/*var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers().AddJsonOptions(o =>
-{
-    o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-});
-builder.Services.AddCors();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(p =>
-    p.AddPolicy("corsapp", builder => { builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader(); }));
-builder.Services.AddScoped<ITripDbRepository, TripDbRepository>();
-
-builder.Services.AddControllers().AddNewtonsoftJson(x =>
- x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString)
-    );
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(jwt =>
-{
-    var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtConfig:Secret").Value);
-    jwt.SaveToken = true;
-    jwt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false, // for dev
-        ValidateAudience = false, // for dev
-        RequireExpirationTime = false, // for dev -- needs to be update when refresh token is added
-        ValidateLifetime = true
-    };
-});
-
-builder.Services.AddDefaultIdentity<User>(options =>
-{
-    options.SignIn.RequireConfirmedEmail = false;
-}).AddEntityFrameworkStores<ApplicationDbContext>();
-
-var app = builder.Build();
-
-// Create a LOG folder with file
-Logger.createLogFile();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-
-    var context = services.GetRequiredService<ApplicationDbContext>();
-    context.Database.EnsureCreated();
-    DbInitializer.Initialize(context);
-}
-
-app.UseCors("corsapp");
-app.UseAuthentication();
-app.UseRouting();
-app.UseAuthorization();
-
-app.UseWhen(context => context.Request.Path.StartsWithSegments("/apisecure"), builder =>
-{
-    builder.UseJwtAuthMiddleware();
-});
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
-
-app.Run();*/
